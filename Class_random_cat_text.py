163fb8f63fb8bba91bf0f875b8acfd001aa54ea1
Arsik2023 <137551937+Arsik2023@users.noreply.github.com>
2023-07-18 18:37:54 +0500

    Add files via upload

---
+++ Class_random_cat_text.py
@@ -0,0 +1,12 @@
+from requests import *
+from pprint import pprint
+
+class Random_cat_photo_text:
+    def __init__(self, text):
+        self.text = text
+    def Random_cat_photo_text_url(self):
+        url = 'https://cataas.com/cat/says/'+self.text+'?json=true'
+        response = get(url).json()
+        return 'https://cataas.com/'+response['url']
+""" a = Random_cat_photo_text('hello')
+print(a.Random_cat_photo_text_url()) """
