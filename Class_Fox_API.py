cc983998b374e4dd60dae85d1bb45fc633bef298
Arsik2023 <137551937+Arsik2023@users.noreply.github.com>
2023-07-23 22:32:13 +0500

    Delete Class_Fox_API.py

--- Class_Fox_API.py
+++
@@ -1,8 +0,0 @@
-from requests import *
-from pprint import pprint
-
-class Random_fox_photo:
-    def __init__(self):
-        pass
-    def Random_fox_url():
-        return get('https://randomfox.ca/floof/').json()['image']
